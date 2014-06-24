fd3357132e1216b610f8847a12be8fd349e07df5
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2014-06-23 23:02:09 -0700

    Use React.addons.classSet for class construction

--- src/day.jsx
+++ src/day.jsx
@@ -1,23 +1,15 @@
 /** @jsx React.DOM */
 
 window.Day = React.createClass({
-  className: function() {
-    classNames = ['day'];
-
-    if(this.props.day.isSame(this.props.month, 'month')) {
-      classNames.push('this-month');
-    }
-
-    if(moment().isSame(this.props.day, 'day')) {
-      classNames.push('today');
-    }
-
-    return classNames.join(' ');
-  },
-
   render: function() {
+    classes = React.addons.classSet({
+      'day': true,
+      'this-month': this.props.day.isSame(this.props.month, 'month'),
+      'today': moment().isSame(this.props.day, 'day')
+    });
+
     return (
-      <div className={this.className()}>{this.props.day.date()}</div>
+      <div className={classes}>{this.props.day.date()}</div>
     );
   }
 });
