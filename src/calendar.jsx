8e1a23f8a908e01a4f9899dda911f068b2beb274
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2014-06-28 15:56:44 -0700

    Removed logicless month

--- src/calendar.jsx
+++ src/calendar.jsx
@@ -23,17 +23,36 @@ window.Calendar = React.createClass({
     return (
       <div className="calendar">
         <div className="calendar-header">
-          <a className="calendar-month-navigation-left" onClick={this.decreaseMonth}>
+          <a className="calendar-month-navigation-left"
+              onClick={this.decreaseMonth}>
             &laquo;
           </a>
           <span className="calendar-current-month">
             {this.state.date.format("MMMM YYYY")}
           </span>
-          <a className="calendar-month-navigation-right" onClick={this.increaseMonth}>
+          <a className="calendar-month-navigation-right"
+              onClick={this.increaseMonth}>
             &raquo;
           </a>
         </div>
-        <Month date={this.state.date} />
+        <div className="month">
+          <div>
+            <div className="day head">MO</div>
+            <div className="day head">TU</div>
+            <div className="day head">WE</div>
+            <div className="day head">TH</div>
+            <div className="day head">FR</div>
+            <div className="day head">SA</div>
+            <div className="day head">SU</div>
+          </div>
+          {
+            this.props.date.mapWeeksInMonth(function(weekStart, key) {
+              return (
+                <Week key={key} week={weekStart} date={this.props.date} />
+              );
+            }, this)
+          }
+        </div>
       </div>
     );
   }
