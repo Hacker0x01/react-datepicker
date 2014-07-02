f61ce0212c37bbbee056e66c2c702b1e742c40a8
Martijn Russchen <martijn@hackerone.com>
2014-07-02 10:16:57 +0200

    Added compression for stylesheets

--- gruntfile.js
+++ gruntfile.js
@@ -3,6 +3,9 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     sass: {
       dist: {
+        options: {
+          style: 'compressed'
+        },
         files: {
           'src/stylesheets/style.css' : 'src/stylesheets/base.scss'
         }
