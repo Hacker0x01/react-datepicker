6ee23b915c09f53dd43dec6a8d182f8b37f16cbb
Remon Oldenbeuving <remon@hackerone.com>
2015-07-29 13:51:10 +0200

    Make JSCS more strict and run on watch

--- gruntfile.js
+++ gruntfile.js
@@ -1,136 +1,145 @@
-'use strict';
+"use strict";
 
-var _ = require('lodash');
-var webpack = require('webpack');
+var _ = require( "lodash" );
+var webpack = require( "webpack" );
 
-var mergeWebpackConfig = function (config) {
+var mergeWebpackConfig = function( config ) {
+
   // Load webpackConfig only when using `grunt:webpack`
   // load of grunt tasks is faster
-  var webpackConfig = require('./webpack.config');
-  return _.merge({}, webpackConfig, config, function (a, b) {
-    if (_.isArray(a)) {
-      return a.concat(b);
+  var webpackConfig = require( "./webpack.config" );
+  return _.merge( {}, webpackConfig, config, function( a, b ) {
+    if ( _.isArray( a ) ) {
+      return a.concat( b );
     }
-  });
+  } );
 };
 
-module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+module.exports = function( grunt ) {
+  grunt.initConfig( {
+    pkg: grunt.file.readJSON( "package.json" ),
     sass: {
       min: {
         files: {
-          'dist/react-datepicker.css': 'src/stylesheets/datepicker.scss'
+          "dist/react-datepicker.css": "src/stylesheets/datepicker.scss"
         },
         options: {
-          sourcemap: 'none',
-          style: 'expanded'
+          sourcemap: "none",
+          style: "expanded"
         }
       },
       unmin: {
         files: {
-          'dist/react-datepicker.min.css': 'src/stylesheets/datepicker.scss'
+          "dist/react-datepicker.min.css": "src/stylesheets/datepicker.scss"
         },
         options: {
-          sourcemap: 'none',
-          style: 'compressed'
+          sourcemap: "none",
+          style: "compressed"
         }
       }
     },
 
     watch: {
+      jscs: {
+        files: [
+          "{src,test}/**/*.{js,jsx}",
+          "gruntfile.js",
+          "karma.conf.js"
+        ],
+        tasks: [ "jscs" ]
+      },
+
       css: {
-        files: '**/*.scss',
-        tasks: ['sass']
+        files: "**/*.scss",
+        tasks: [ "sass" ]
       },
 
       webpack: {
-        files: ['src/**/*.js', 'src/**/*.jsx'],
-        tasks: ['webpack']
+        files: [ "src/**/*.js", "src/**/*.jsx" ],
+        tasks: [ "webpack" ]
       }
     },
 
     scsslint: {
-      files: 'src/stylesheets/*.scss',
+      files: "src/stylesheets/*.scss",
       options: {
-        config: '.scss-lint.yml',
+        config: ".scss-lint.yml",
         colorizeOutput: true
       }
     },
 
     karma: {
       unit: {
-        configFile: 'karma.conf.js',
+        configFile: "karma.conf.js",
         singleRun: true
       }
     },
 
-
     jscs: {
       files: [
-        'src/**/*.jsx',
-        'src/**/*.js',
-        'test/**/*.jsx',
-        'test/**/*.js'
+        "{src,test}/**/*.{js,jsx}",
+        "gruntfile.js",
+        "karma.conf.js"
       ],
       options: {
-        config: '.jscsrc',
-        verbose: true
+        config: ".jscsrc",
+        verbose: true,
+        fix: grunt.option( "fix" ) || false
       }
     },
 
     webpack: {
       example: {
-        entry: './example/boot',
+        entry: "./example/boot",
         output: {
-          filename: 'example.js',
-          library: 'ExampleApp',
-          path: './example/'
+          filename: "example.js",
+          library: "ExampleApp",
+          path: "./example/"
         },
         resolve: {
-          extensions: ['', '.js', '.jsx']
+          extensions: [ "", ".js", ".jsx" ]
         },
         module: {
           loaders: [
-            {test: /\.js/, loaders: ['babel?stage=0'], exclude: /node_modules/}
+            { test: /\.js/, loaders: [ "babel?stage=0" ], exclude: /node_modules/ }
           ]
         },
-        node: {Buffer: false},
+        node: { Buffer: false },
         plugins: [
           new webpack.optimize.DedupePlugin(),
-          new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-          })
+          new webpack.DefinePlugin( {
+            "process.env.NODE_ENV": JSON.stringify( process.env.NODE_ENV )
+          } )
         ]
       },
-      unmin: mergeWebpackConfig({
+      unmin: mergeWebpackConfig( {
         output: {
-          filename: 'react-datepicker.js'
+          filename: "react-datepicker.js"
         }
-      }),
-      min: mergeWebpackConfig({
+      } ),
+      min: mergeWebpackConfig( {
         output: {
-          filename: 'react-datepicker.min.js'
+          filename: "react-datepicker.min.js"
         },
         plugins: [
-          new webpack.optimize.UglifyJsPlugin({
+          new webpack.optimize.UglifyJsPlugin( {
             compressor: {
               warnings: false
             }
-          })
+          } )
         ]
-      })
+      } )
     }
-  });
+  } );
 
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-scss-lint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-webpack');
-  grunt.loadNpmTasks('grunt-karma');
-  grunt.loadNpmTasks("grunt-jscs");
+  grunt.loadNpmTasks( "grunt-contrib-sass" );
+  grunt.loadNpmTasks( "grunt-scss-lint" );
+  grunt.loadNpmTasks( "grunt-contrib-watch" );
+  grunt.loadNpmTasks( "grunt-webpack" );
+  grunt.loadNpmTasks( "grunt-karma" );
+  grunt.loadNpmTasks( "grunt-jscs" );
 
-  grunt.registerTask('default', ['watch', 'scsslint']);
-  grunt.registerTask('travis', ['jscs', 'karma', 'scsslint']);
-  grunt.registerTask('build', ['scsslint', 'webpack', 'sass']);
+  grunt.registerTask( "default", [ "watch", "scsslint" ] );
+  grunt.registerTask( "travis", [ "jscs", "karma", "scsslint" ] );
+  grunt.registerTask( "build", [ "scsslint", "webpack", "sass" ] );
 };
--- karma.conf.js
+++ karma.conf.js
@@ -1,45 +1,45 @@
 // Karma configuration
 // Generated on Tue Jul 28 2015 16:29:51 GMT+0200 (CEST)
-var webpack = require('webpack');
+var webpack = require( "webpack" );
 
-module.exports = function(config) {
-  config.set({
-    frameworks: ['mocha', 'sinon', 'chai'],
+module.exports = function( config ) {
+  config.set( {
+    frameworks: [ "mocha", "sinon", "chai" ],
 
-    browsers: [ process.env.CONTINUOUS_INTEGRATION ? 'Firefox' : 'Chrome' ],
+    browsers: [ process.env.CONTINUOUS_INTEGRATION ? "Firefox" : "Chrome" ],
 
-    singleRun: process.env.CONTINUOUS_INTEGRATION === 'true',
+    singleRun: process.env.CONTINUOUS_INTEGRATION === "true",
 
     files: [
-      'test/index.js'
+      "test/index.js"
     ],
 
     preprocessors: {
-      'test/index.js': [ 'webpack', 'sourcemap' ]
+      "test/index.js": [ "webpack", "sourcemap" ]
     },
 
-    reporters: [ 'dots' ],
+    reporters: [ "dots" ],
 
     webpack: {
-      devtool: 'inline-source-map',
+      devtool: "inline-source-map",
       module: {
         loaders: [
           {
             test: /\.jsx?$/,
             exclude: /(node_modules|bower_components)/,
-            loader: 'babel'
+            loader: "babel"
           }
         ]
       },
       plugins: [
-        new webpack.DefinePlugin({
-          'process.env.NODE_ENV': JSON.stringify('test')
-        })
+        new webpack.DefinePlugin( {
+          "process.env.NODE_ENV": JSON.stringify( "test" )
+        } )
       ]
     },
 
     webpackServer: {
       noInfo: true
-    },
-  })
-}
+    }
+  } );
+};
