62c5b671a644792e83834f2bc4b898d96adbc63e
Martijn Russchen <martijn@hackerone.com>
2015-07-29 20:04:05 +0200

    Move examples to JS

--- src/app.jsx
+++ src/app.jsx
@@ -79,65 +79,117 @@ var exampleComponent = React.createClass({
 
   render: function() {
     return <div>
-      <DatePicker
-        key="example1"
-        selected={this.state.start_date}
-        onChange={this.handleStartDateChange}
-      />
-      <DatePicker
-        key="example2"
-        dateFormat="YYYY/MM/DD"
-        selected={this.state.end_date}
-        onChange={this.handleEndDateChange}
-      />
-      <DatePicker
-        key="example3"
-        selected={this.state.new_date}
-        onChange={this.handleNewDateChange}
-        placeholderText="Click to select a date"
-      />
-      <DatePicker
-        key="example4"
-        selected={this.state.bound_date}
-        onChange={this.handleBoundDateChange}
-        minDate={moment()}
-        maxDate={moment().add(5, 'days')}
-        placeholderText="Select a date between today and 5 days in the future"
-      />
-      <DatePicker
-        key="example5"
-        selected={this.state.example5Selected}
-        onChange={this.handleExample5Change}
-        weekStart="0"
-        placeholderText="I start on Sunday!"
-      />
-      <DatePicker
-        key="example6"
-        selected={this.state.example6Selected}
-        onChange={this.handleExample6Change}
-        excludeDates={[moment(), moment().subtract(1, 'days')]}
-        placeholderText="Select a date other than today or yesterday"
-      />
-      <DatePicker
-        key="example7"
-        selected={null}
-        onChange={this.handleStartDateChange}
-        disabled={true}
-        placeholderText="This is disabled"
-      />
-      <DatePicker
-        key="example8"
-        selected={this.state.example8Selected}
-      />
-      <button onClick={this.handleClearButtonClick}>Clear</button>
+      <div className="example">
+        <div className="column"></div>
+        <div className="column">
+          <DatePicker
+            key="example1"
+            selected={this.state.start_date}
+            onChange={this.handleStartDateChange}
+          />
+        </div>
+      </div>
 
-      <DatePicker
-        key="example9"
-        selected={this.state.example9Selected}
-        onChange={this.handleExample9Change}
-        onBlur={this.handleExample9Blur}
-        placeholderText="View blur callbacks in console"
-      />
+      <div className="example">
+        <div className="column"></div>
+        <div className="column">
+          <DatePicker
+            key="example2"
+            dateFormat="YYYY/MM/DD"
+            selected={this.state.end_date}
+            onChange={this.handleEndDateChange}
+          />
+        </div>
+      </div>
+
+      <div className="example">
+        <div className="column"></div>
+        <div className="column">
+          <DatePicker
+            key="example3"
+            selected={this.state.new_date}
+            onChange={this.handleNewDateChange}
+            placeholderText="Click to select a date"
+          />
+        </div>
+      </div>
+
+      <div className="example">
+        <div className="column"></div>
+        <div className="column">
+          <DatePicker
+            key="example4"
+            selected={this.state.bound_date}
+            onChange={this.handleBoundDateChange}
+            minDate={moment()}
+            maxDate={moment().add(5, 'days')}
+            placeholderText="Select a date between today and 5 days in the future"
+          />
+        </div>
+      </div>
+
+      <div className="example">
+        <div className="column"></div>
+        <div className="column">
+          <DatePicker
+            key="example5"
+            selected={this.state.example5Selected}
+            onChange={this.handleExample5Change}
+            weekStart="0"
+            placeholderText="I start on Sunday!"
+          />
+        </div>
+      </div>
+
+      <div className="example">
+        <div className="column"></div>
+        <div className="column">
+          <DatePicker
+            key="example6"
+            selected={this.state.example6Selected}
+            onChange={this.handleExample6Change}
+            excludeDates={[moment(), moment().subtract(1, 'days')]}
+            placeholderText="Select a date other than today or yesterday"
+          />
+        </div>
+      </div>
+
+      <div className="example">
+        <div className="column"></div>
+        <div className="column">
+          <DatePicker
+            key="example7"
+            selected={null}
+            onChange={this.handleStartDateChange}
+            disabled={true}
+            placeholderText="This is disabled"
+          />
+        </div>
+      </div>
+
+      <div className="example">
+        <div className="column"></div>
+        <div className="column">
+          <DatePicker
+            key="example8"
+            selected={this.state.example8Selected}
+          />
+          <button onClick={this.handleClearButtonClick}>Clear</button>
+        </div>
+      </div>
+
+      <div className="example">
+        <div className="column"></div>
+        <div className="column">
+          <DatePicker
+            key="example9"
+            selected={this.state.example9Selected}
+            onChange={this.handleExample9Change}
+            onBlur={this.handleExample9Blur}
+            placeholderText="View blur callbacks in console"
+          />
+        </div>
+      </div>
     </div>;
   }
 });
