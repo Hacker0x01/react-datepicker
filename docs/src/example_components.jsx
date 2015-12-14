3489a5df3f2948c4a00a32bdf8c1c1612116984e
Martijn Russchen <martijn@hackerone.com>
2015-12-14 10:50:16 +0100

    Fix links for left sidebar

--- docs/src/example_components.jsx
+++ docs/src/example_components.jsx
@@ -25,7 +25,7 @@ const CodeExampleComponent = React.createClass({
   displayName: "CodeExampleComponent",
 
   render() {
-    return <div key={this.props.id} id={`#example-${this.props.id}`} className="example">
+    return <div key={this.props.id} id={`example-${this.props.id}`} className="example">
       <h2 className="example__heading">{this.props.title}</h2>
       {this.props.children}
     </div>;
