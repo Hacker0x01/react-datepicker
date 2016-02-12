89caed012375e529cda9f9307455c188d1b6121a
Martijn Russchen <martijn@hackerone.com>
2016-02-12 14:29:14 +0100

    Fix build script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# npm version $1
+npm version $1
 
 react-docgen ./src/*.jsx | ./scripts/buildDocs.sh
 
@@ -8,10 +8,10 @@ git add .
 
 git commit -m "Publish new API docs (automated commit)"
 
-# git push
-#
-# git push --tags
-#
-# npm publish
+git push
 
-# ./docs-site/publish.sh
+git push --tags
+
+npm publish
+
+./docs-site/publish.sh
