8ee659e49789c87de2d1453e4e72d2d3f5b3efab
Martijn Russchen <martijn@hackerone.com>
2016-03-04 19:42:06 +0100

    Also add propTypes for the docs-site

--- docs-site/src/code_example_component.jsx
+++ docs-site/src/code_example_component.jsx
@@ -3,6 +3,12 @@ import React from 'react'
 export default React.createClass({
   displayName: 'CodeExampleComponent',
 
+  propTypes: {
+    id: React.propTypes.string,
+    title: React.propTypes.string,
+    children: React.propTypes.element
+  },
+
   render () {
     return <div key={this.props.id} id={`example-${this.props.id}`} className="example">
       <h2 className="example__heading">{this.props.title}</h2>
