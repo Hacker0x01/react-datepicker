27e629569b147bb207341d705f58b54718f38081
Jared Sohn <jared.sohn@gmail.com>
2016-06-29 18:29:15 -0700

    Support a couple more formats

--- src/date_input.jsx
+++ src/date_input.jsx
@@ -87,12 +87,14 @@ var DateInput = React.createClass({
       "MMM D, YY",
       "MMMM D, YYYY",
       "MMMM D, YY",
+      "MMM/D/YYYY",
 
       // without commas
       "MMM D YYYY",
       "MMM D YY",
       "MMMM D YYYY",
       "MMMM D YY",
+      "MMM-D-YYYY",
 
       "MM-DD-YYYY",
       "MM/DD/YYYY",
