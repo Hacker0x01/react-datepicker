0d8face4e9c058828fb6d054e26bfd1928500346
meggantodd <meggantodd@gmail.com>
2016-07-08 14:20:54 -0400

    React "Unknown Prop Warning" fix (#522)
    * Fix for React "Unknown Prop Warning." Do not pass unsupported props to <input>.
    
    * Ignore eslint warning for no-unused-vars

--- src/date_input.jsx
+++ src/date_input.jsx
@@ -83,10 +83,11 @@ var DateInput = React.createClass({
   },
 
   render () {
+    const { date, locale, minDate, maxDate, excludeDates, includeDates, filterDate, dateFormat, onChangeDate, ...rest } = this.props // eslint-disable-line no-unused-vars
     return <input
         ref='input'
         type='text'
-        {...this.props}
+        {...rest}
         value={this.state.value}
         onBlur={this.handleBlur}
         onChange={this.handleChange} />
