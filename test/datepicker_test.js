8b625c725fc99fcbe00a52217c72ca1ce90c9611
MarkReeder <mark@arcticflare.com>
2016-10-05 06:55:55 -0700

    Remove autofocus test defer.

--- test/datepicker_test.js
+++ test/datepicker_test.js
@@ -298,13 +298,10 @@ describe('DatePicker', () => {
     var result = data.callback.args[0][0]
     expect(result.format(data.testFormat)).to.equal(data.copyM.format(data.testFormat))
   })
-  it('should autofocus the input given the autoFocus prop', (done) => {
+  it('should autofocus the input given the autoFocus prop', () => {
     var div = document.createElement('div')
     document.body.appendChild(div)
     ReactDOM.render(<DatePicker autoFocus />, div)
-    defer(() => {
-      expect(div.querySelector('input')).to.equal(document.activeElement)
-      done()
-    })
+    expect(div.querySelector('input')).to.equal(document.activeElement)
   })
 })
