4f887469fffa2401fa0c43325c8d4e7901aa0cad
Ivan Jager <ivan.jager@wicourts.gov>
2017-01-17 12:32:58 -0600

    Also upgrade from grunt-contrib-sass to grunt-sass
    grunt-sass uses node-sass, whereas grunt-contrib-sass
    still depends on Ruby.

--- gruntfile.js
+++ gruntfile.js
@@ -133,7 +133,7 @@ module.exports = function (grunt) {
     }
   })
 
-  grunt.loadNpmTasks('grunt-contrib-sass')
+  grunt.loadNpmTasks('grunt-sass')
   grunt.loadNpmTasks('grunt-sass-lint')
   grunt.loadNpmTasks('grunt-contrib-watch')
   grunt.loadNpmTasks('grunt-babel')
