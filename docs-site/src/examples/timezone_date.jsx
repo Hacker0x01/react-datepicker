ec0a13ac774d17d022feef77aae714700cddf807
Greg Smith <greg.a.smith@concur.com>
2017-07-03 15:05:55 -0500

    Take utcOffset into account when creating a defaultPreSelection date

--- docs-site/src/examples/timezone_date.jsx
+++ docs-site/src/examples/timezone_date.jsx
@@ -62,8 +62,6 @@ export default class TimeZoneDate extends React.Component {
             dateFormat="DD-MMM YYYY HH:mm"
             todayButton={todayTxt}
             selected={selected}
-            minDate={moment('2016-11-05T00:00:00+00:00').utcOffset(this.state.utcOffset)}
-            maxDate={moment('2016-12-04T00:00:00-04:00').utcOffset(this.state.utcOffset)}
             onChange={this.handleChange} />
         <br/>
         <label className="example__timezone-label">
