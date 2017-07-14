b5f9bfd53fcd3a38eecd430ba568ce5fcc15af5a
Martijn Russchen <martijn@factlink.com>
2017-07-14 15:35:08 +0200

    Fix lint error

--- src/datepicker.jsx
+++ src/datepicker.jsx
@@ -263,7 +263,7 @@ export default class DatePicker extends React.Component {
   }
 
   onInputKeyDown = (event) => {
-    this.props.onKeyDown(event);
+    this.props.onKeyDown(event)
     const eventKey = event.key
     if (!this.state.open && !this.props.inline) {
       if (eventKey !== 'Enter' && eventKey !== 'Escape' && eventKey !== 'Tab') {
