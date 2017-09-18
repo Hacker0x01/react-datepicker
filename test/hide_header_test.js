c1109a87ae2870fb43ba569cab3326eb2f9a9983
Yaron Nachshon <yaronn@wix.com>
2017-09-18 14:25:04 +0300

    chore(test):  check the default visibility of the header

--- test/hide_header_test.js
+++ test/hide_header_test.js
@@ -7,14 +7,16 @@ describe.only('Calendar', () => {
     return mount(
       <Calendar
           dateFormat="MMMM YYYY"
-          onSelect={() => {}}
-          onClickOutside={() => {}}
-          hideCalendar={() => {}}
-          hideHeader
-          dropdownMode="scroll"
           {...extraProps}/>
     )
   }
+
+  it('should show header by default', () => {
+    var calendar = getCalendar({})
+    expect(calendar.find('.react-datepicker__current-month').length).to.equal(1)
+    expect(calendar.find('.react-datepicker__navigation').length).to.equal(2)
+  })
+
   it('should show header specified in hideHeader props', () => {
     var calendar = getCalendar({
       hideHeader: false
