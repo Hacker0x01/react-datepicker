e71933075e33a3b124c2917dadccc9c87a84fb0f
Adam Golab <adam@brainhub.pl>
2017-10-01 20:14:08 +0200

    Migrated to react 16.0.0

--- test/exclude_time_period_test.js
+++ test/exclude_time_period_test.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import moment from 'moment'
-// import ReactDOM from 'react-dom'
-// import TestUtils from 'react-addons-test-utils'
 import { mount } from 'enzyme'
 import DatePicker from '../src/datepicker.jsx'
-// import Time from '../src/time.jsx'
 
 describe('DatePicker', () => {
   let sandbox
