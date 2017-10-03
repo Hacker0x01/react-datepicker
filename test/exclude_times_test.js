fbf542bf03841390d3aaaf4277c06fe8bf587d0c
Nikolaus Piccolotto <nikolaus.piccolotto@artory.com>
2017-10-03 15:37:19 +0200

    Remove moment from exclude_times_test

--- test/exclude_times_test.js
+++ test/exclude_times_test.js
@@ -1,7 +1,11 @@
 import React from 'react'
-import moment from 'moment'
 import { mount } from 'enzyme'
 import DatePicker from '../src/datepicker.jsx'
+import * as utils from '../src/date_utils'
+
+function cloneDateWithTime (date, time) {
+  return utils.setTime(utils.cloneDate(date), time)
+}
 
 describe('DatePicker', () => {
   let sandbox
@@ -15,10 +19,16 @@ describe('DatePicker', () => {
   })
 
   it('should disable times specified in excludeTimes props', () => {
+    var now = utils.newDate()
     var datePicker = mount(
       <DatePicker
           showTimeSelect
-          excludeTimes={[moment().hours(17).minutes(0), moment().hours(18).minutes(30), moment().hours(19).minutes(30), moment().hours(17).minutes(30)]} />
+          excludeTimes={[
+            cloneDateWithTime(now, { hours: 17, minutes: 0 }),
+            cloneDateWithTime(now, { hours: 18, minutes: 30 }),
+            cloneDateWithTime(now, { hours: 19, minutes: 30 }),
+            cloneDateWithTime(now, { hours: 17, minutes: 30 })
+          ]} />
     )
     expect(datePicker.find('.react-datepicker__time-list-item--disabled')).to.exist
   })
