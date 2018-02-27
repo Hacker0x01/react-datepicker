da88933d83f58b8c3af885a1676e350e89f3496f
Greg Smith <greg.a.smith@concur.com>
2018-02-27 10:15:57 -0600

    Added additional unit tests

--- test/show_time_test.js
+++ test/show_time_test.js
@@ -27,9 +27,29 @@ describe("DatePicker", () => {
     expect(caption.text()).to.equal("Custom time");
   });
 
-  it("should not show date component when showTimeSelectOnly prop is present", () => {
-    var datePicker = mount(<DatePicker showTimeSelect showTimeSelectOnly />);
-    var monthContainer = datePicker.find(".react-datepicker__month-container");
-    expect(monthContainer.length).to.equal(0);
+  describe("Time Select Only", () => {
+    var datePicker = mount(
+      <DatePicker showTimeSelect showTimeSelectOnly todayButton="Today" />
+    );
+
+    it("should not show month container when showTimeSelectOnly prop is present", () => {
+      var elem = datePicker.find(".react-datepicker__month-container");
+      expect(elem).to.have.length(0);
+    });
+
+    it("should not show previous month button when showTimeSelectOnly prop is present", () => {
+      var elem = datePicker.find(".react-datepicker__navigation--previous");
+      expect(elem).to.have.length(0);
+    });
+
+    it("should not show next month button when showTimeSelectOnly prop is present", () => {
+      var elem = datePicker.find(".react-datepicker__navigation--next");
+      expect(elem).to.have.length(0);
+    });
+
+    it("should not show today button when showTimeSelectOnly prop is present", () => {
+      var elem = datePicker.find(".react-datepicker__today-button");
+      expect(elem).to.have.length(0);
+    });
   });
 });
