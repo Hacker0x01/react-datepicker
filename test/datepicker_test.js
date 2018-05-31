b68be3b851892b44133a6e494e3c8d7dbf7a4eaf
Ivan Jager <aij+git@mrph.org>
2018-05-31 09:02:45 -0500

    Fix flaky test again

--- test/datepicker_test.js
+++ test/datepicker_test.js
@@ -241,9 +241,12 @@ describe("DatePicker", () => {
   it("should update the preSelection state when a day is selected with mouse click", () => {
     // Note: We need monthsShown=2 so that today can still be clicked when
     // ArrowLeft selects the previous month. (On the 1st 2 days of the month.)
+    // On the last week of the month, when the next month includes the current
+    // week, we need monthsShown=1 to prevent today from appearing twice.
+    const dayOfMonth = utils.getDate(utils.now());
     var data = getOnInputKeyDownStuff({
       shouldCloseOnSelect: false,
-      monthsShown: 2
+      monthsShown: dayOfMonth < 15 ? 2 : 1
     });
 
     TestUtils.Simulate.keyDown(data.nodeInput, getKey("ArrowLeft"));
