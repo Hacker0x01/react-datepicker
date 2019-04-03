01bbdfa3644e29dc4e201dfa2fa54684f20e00c8
igoradamenko <mail@igoradamenko.com>
2019-04-03 19:55:24 +0300

    Add `transform-react-remove-prop-types` to babel config for production builds.

--- .babelrc.js
+++ .babelrc.js
@@ -1,41 +1,45 @@
 var NODE_ENV = process.env.NODE_ENV;
 var MODULES = process.env.MODULES;
 
-var modules = MODULES === 'false' || NODE_ENV === 'test' ? 'commonjs' : false;
+var modules = MODULES === "false" || NODE_ENV === "test" ? "commonjs" : false;
 
 var config = {
   presets: [
     [
-      'env',
+      "env",
       {
         loose: true,
         modules: modules,
-        forceAllTransforms: NODE_ENV === 'production',
-      },
+        forceAllTransforms: NODE_ENV === "production"
+      }
     ],
-    'stage-0',
-    'react',
+    "stage-0",
+    "react"
   ],
-  plugins: [],
+  plugins: []
 };
 
-if (NODE_ENV === 'development') {
+if (NODE_ENV === "development") {
   config.plugins = config.plugins.concat([
-    'transform-class-properties',
+    "transform-class-properties",
     [
-      'react-transform',
+      "react-transform",
       {
         transforms: [
           {
-            transform: 'react-transform-hmr',
-            imports: ['react'],
-            locals: ['module'],
-          },
-        ],
-      },
+            transform: "react-transform-hmr",
+            imports: ["react"],
+            locals: ["module"]
+          }
+        ]
+      }
     ],
-    'add-react-displayname',
+    "add-react-displayname"
   ]);
+}
+
+if (NODE_ENV === "production") {
+  config.plugins = config.plugins.concat(["transform-react-remove-prop-types"]);
 }
 
 module.exports = config;
