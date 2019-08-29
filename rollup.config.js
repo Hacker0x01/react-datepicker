f505c11bc75fab0fd299714539abaf788d008bd0
Max Schmitt <max@schmitt.mx>
2019-08-29 15:01:08 +0200

    feat: enhanced rollup configuration (#1883)
    * feat: enhanced rollup configuration
    
    * feat: cleaned up

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,15 @@
 import fs from "fs";
 import path from "path";
 
-import nodeResolve from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
-import { list as babelHelpersList } from "@babel/helpers";
+import filesize from "rollup-plugin-filesize";
+import localResolve from "rollup-plugin-local-resolve";
+import { terser } from "rollup-plugin-terser";
+
+import replace from "rollup-plugin-replace";
 import pkg from "./package.json";
 
 // it's important to mark all subpackages of data-fns as externals
@@ -13,27 +18,53 @@ const dateFnsDirs = fs
   .readdirSync(path.join(".", "node_modules", "date-fns"))
   .map(d => `date-fns/${d}`);
 
+const globals = {
+  react: "React",
+  "prop-types": "PropTypes",
+  "react-onclickoutside": "onClickOutside",
+  "react-popper": "ReactPopper",
+  classnames: "classNames"
+};
+
 const config = {
-  output: {
-    format: process.env.BABEL_ENV
-  },
+  input: "src/index.jsx",
+  output: [
+    {
+      file: pkg.browser,
+      format: "umd",
+      name: "DatePicker",
+      globals
+    },
+    {
+      file: "dist/react-datepicker.js",
+      format: "umd",
+      name: "DatePicker",
+      globals
+    },
+    {
+      file: pkg.main,
+      format: "cjs",
+      name: "DatePicker"
+    },
+    {
+      file: pkg.module,
+      format: "es"
+    }
+  ],
   plugins: [
-    nodeResolve({
+    resolve({
       mainFields: ["module"],
       extensions: [".js", ".jsx"]
     }),
-    babel({
-      exclude: "node_modules/**",
-      plugins: [
-        "@babel/plugin-external-helpers",
-        "@babel/plugin-proposal-class-properties"
-      ],
-      runtimeHelpers: true,
-      externalHelpersWhitelist: babelHelpersList.filter(
-        helperName => helperName !== "asyncGenerator"
-      )
-    }),
-    commonjs()
+    peerDepsExternal(),
+    babel(),
+    localResolve(),
+    commonjs(),
+    filesize(),
+    terser(),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("production")
+    })
   ],
   external: Object.keys(pkg.dependencies)
     .concat(Object.keys(pkg.peerDependencies))
