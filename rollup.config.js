34ee72b4fb0503c29fd1b386512eef7449529d98
Max Schmitt <max@schmitt.mx>
2019-08-29 14:41:57 +0200

    feat: cleaned up

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,6 @@
+import fs from "fs";
+import path from "path";
+
 import babel from "rollup-plugin-babel";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "rollup-plugin-node-resolve";
@@ -5,10 +8,15 @@ import commonjs from "rollup-plugin-commonjs";
 import filesize from "rollup-plugin-filesize";
 import localResolve from "rollup-plugin-local-resolve";
 import { terser } from "rollup-plugin-terser";
-import visualizer from "rollup-plugin-visualizer";
 
 import replace from "rollup-plugin-replace";
 import pkg from "./package.json";
+
+// it's important to mark all subpackages of data-fns as externals
+// see https://github.com/Hacker0x01/react-datepicker/issues/1606
+const dateFnsDirs = fs
+  .readdirSync(path.join(".", "node_modules", "date-fns"))
+  .map(d => `date-fns/${d}`);
 
 const globals = {
   react: "React",
@@ -57,7 +65,10 @@ const config = {
     replace({
       "process.env.NODE_ENV": JSON.stringify("production")
     })
-  ]
+  ],
+  external: Object.keys(pkg.dependencies)
+    .concat(Object.keys(pkg.peerDependencies))
+    .concat(dateFnsDirs)
 };
 
 export default config;
