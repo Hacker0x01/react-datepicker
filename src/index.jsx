b943f20cd6e0f7bbd1b03a9e4c293f14cc29d06d
Mateusz Krzyżanowski <matkrzy94@gmail.com>
2019-11-04 16:01:28 +0100

    Feature/calendar status handlers (#1947)
    * Add onCalendarOpen and onCalendarClose handlers to determine calendar state
    
    * Fix build settings and the main path

--- src/index.jsx
+++ src/index.jsx
@@ -111,6 +111,8 @@ export default class DatePicker extends React.Component {
     onYearChange: PropTypes.func,
     onInputError: PropTypes.func,
     open: PropTypes.bool,
+    onCalendarOpen: PropTypes.func,
+    onCalendarClose: PropTypes.func,
     openToDate: PropTypes.instanceOf(Date),
     peekNextMonth: PropTypes.bool,
     placeholderText: PropTypes.string,
@@ -190,6 +192,8 @@ export default class DatePicker extends React.Component {
       onSelect() {},
       onClickOutside() {},
       onMonthChange() {},
+      onCalendarOpen() {},
+      onCalendarClose() {},
       preventOpenOnFocus: false,
       onYearChange() {},
       onInputError() {},
@@ -247,6 +251,16 @@ export default class DatePicker extends React.Component {
       !isEqual(prevProps.selected, this.props.selected)
     ) {
       this.setState({ inputValue: null });
+    }
+
+    if (prevState.open !== this.state.open) {
+      if (prevState.open === false && this.state.open === true) {
+        this.props.onCalendarOpen();
+      }
+
+      if (prevState.open === true && this.state.open === false) {
+        this.props.onCalendarClose();
+      }
     }
   }
 
