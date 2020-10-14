b656619d9daaf5840f8a21c8171157be3ce54ed3
mroforolhc <mroforolhc@gmail.com>
2020-10-14 11:47:15 +0300

    change the example to be more descriptive

--- docs-site/src/examples/filterTimes.js
+++ docs-site/src/examples/filterTimes.js
@@ -2,16 +2,18 @@
   const [startDate, setStartDate] = useState(
     setHours(setMinutes(new Date(), 0), 9)
   );
-  const from9to5 = time => {
-    const hour = getHours(time);
-    return hour >= 9 && hour < 17;
+  const filterPassedTime = time => {
+    const currentDate = new Date();
+    const selectedDate = new Date(time);
+
+    return currentDate.getTime() < selectedDate.getTime();
   }
   return (
     <DatePicker
       selected={startDate}
       onChange={date => setStartDate(date)}
       showTimeSelect
-      filterTime={from9to5}
+      filterTime={filterPassedTime}
       dateFormat="MMMM d, yyyy h:mm aa"
     />
   );
