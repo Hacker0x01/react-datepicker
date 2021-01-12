22a30fe905dfd3901fc92dee3ec27b90f5ac974f
Martijn Russchen <mrusschen@gmail.com>
2021-01-12 08:53:59 +0100

    Revert more changes

--- docs-site/src/examples/configurePopper.js
+++ docs-site/src/examples/configurePopper.js
@@ -3,22 +3,20 @@
   return (
     <DatePicker
       selected={startDate}
-      onChange={(date) => setStartDate(date)}
+      onChange={date => setStartDate(date)}
       popperClassName="some-custom-class"
       popperPlacement="top-end"
-      popperModifiers={[
-        {
-          offset: {
-            enabled: true,
-            offset: "5px, 10px",
-          },
-          preventOverflow: {
-            enabled: true,
-            escapeWithReference: false,
-            boundariesElement: "viewport",
-          },
+      popperModifiers={{
+        offset: {
+          enabled: true,
+          offset: "5px, 10px"
         },
-      ]}
+        preventOverflow: {
+          enabled: true,
+          escapeWithReference: false,
+          boundariesElement: "viewport"
+        }
+      }}
     />
   );
 };
--- src/popper_component.jsx
+++ src/popper_component.jsx
@@ -1,29 +1,29 @@
 import classnames from "classnames";
 import React from "react";
 import PropTypes from "prop-types";
-import { Manager, Reference, Popper } from "react-popper";
-import { placements } from "@popperjs/core/lib";
+import { Manager, Reference, Popper, placements } from "react-popper";
 import TabLoop from "./tab_loop";
 import Portal from "./portal";
 
 export const popperPlacementPositions = placements;
+<<<<<<< HEAD
 const datepickerTriangleSize = 8;
+=======
+>>>>>>> parent of d9d5fbb... chore!: upgrade react-popper
 
 export default class PopperComponent extends React.Component {
   static get defaultProps() {
     return {
       hidePopper: true,
-      popperModifiers: [
-        {
-          preventOverflow: {
-            enabled: true,
-            escapeWithReference: true,
-            boundariesElement: "viewport",
-          },
-        },
-      ],
+      popperModifiers: {
+        preventOverflow: {
+          enabled: true,
+          escapeWithReference: true,
+          boundariesElement: "viewport"
+        }
+      },
       popperProps: {},
-      popperPlacement: "bottom-start",
+      popperPlacement: "bottom-start"
     };
   }
 
@@ -32,14 +32,14 @@ export default class PopperComponent extends React.Component {
     wrapperClassName: PropTypes.string,
     hidePopper: PropTypes.bool,
     popperComponent: PropTypes.element,
-    popperModifiers: PropTypes.arrayOf(PropTypes.object), // <datepicker/> props
+    popperModifiers: PropTypes.object, // <datepicker/> props
     popperPlacement: PropTypes.oneOf(popperPlacementPositions), // <datepicker/> props
     popperContainer: PropTypes.func,
     popperProps: PropTypes.object,
     targetComponent: PropTypes.element,
     enableTabLoop: PropTypes.bool,
     popperOnKeyDown: PropTypes.func,
-    portalId: PropTypes.string,
+    portalId: PropTypes.string
   };
 
   render() {
@@ -54,7 +54,7 @@ export default class PopperComponent extends React.Component {
       targetComponent,
       enableTabLoop,
       popperOnKeyDown,
-      portalId,
+      portalId
     } = this.props;
 
     let popper;
@@ -97,12 +97,21 @@ export default class PopperComponent extends React.Component {
         </Popper>
       );
     }
+<<<<<<< HEAD
     if (this.props.popperContainer) {
       popper = (
         <this.props.popperContainer>{popper}</this.props.popperContainer>
       );
     }
     if (!hidePopper && portalId) {
+=======
+
+    if (this.props.popperContainer) {
+      popper = React.createElement(this.props.popperContainer, {}, popper);
+    }
+
+    if (portalId && !hidePopper) {
+>>>>>>> parent of d9d5fbb... chore!: upgrade react-popper
       popper = <Portal portalId={portalId}>{popper}</Portal>;
     }
 
@@ -112,7 +121,7 @@ export default class PopperComponent extends React.Component {
     );
 
     return (
-      <Manager>
+      <Manager className="react-datepicker-manager">
         <Reference>
           {({ ref }) => (
             <div ref={ref} className={wrapperClasses}>
