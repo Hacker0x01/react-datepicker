b82ed1185bc237f30b0216bd1f12dd1f980a7552
Ties Westendorp <ties.westendorp@nedap.com>
2021-02-26 13:57:47 +0100

    Add weekPicker attribute

--- src/calendar.jsx
+++ src/calendar.jsx
@@ -125,6 +125,7 @@ export default class Calendar extends React.Component {
     showFourColumnMonthYearPicker: PropTypes.bool,
     showYearPicker: PropTypes.bool,
     showQuarterYearPicker: PropTypes.bool,
+    showWeekPicker: PropTypes.bool,
     showTimeSelectOnly: PropTypes.bool,
     timeFormat: PropTypes.string,
     timeIntervals: PropTypes.number,
@@ -280,7 +281,7 @@ export default class Calendar extends React.Component {
   handleDayClick = (day, event, monthSelectedIn) => {
     this.props.onSelect(day, event, monthSelectedIn);
     this.props.setPreSelection && this.props.setPreSelection(day);
-  }   
+  }
 
   handleDayMouseEnter = day => {
     this.setState({ selectingDate: day });
@@ -829,6 +830,7 @@ export default class Calendar extends React.Component {
             }
             showYearPicker={this.props.showYearPicker}
             showQuarterYearPicker={this.props.showQuarterYearPicker}
+            showWeekPicker={this.props.showWeekPicker}
             isInputFocused={this.props.isInputFocused}
             containerRef={this.containerRef}
             monthShowsDuplicateDaysEnd={monthShowsDuplicateDaysEnd}
