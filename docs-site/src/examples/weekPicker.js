49806e1941e3aa16b526685ffa126b23dc387654
Ties Westendorp <ties.westendorp@nedap.com>
2021-03-01 12:36:23 +0100

    Update example

--- docs-site/src/examples/weekPicker.js
+++ docs-site/src/examples/weekPicker.js
@@ -1,5 +1,5 @@
 () => {
-  const [startDate, setStartDate] = useState(new Date("2021/02/28"));
+  const [startDate, setStartDate] = useState(new Date("2021/02/22"));
   return (
     <DatePicker
       selected={startDate}
