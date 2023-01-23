59658cdcca72689cd3fa7311ae95d185627f6328
Már Örlygsson <mar.nospam@anomy.net>
2023-01-23 09:16:21 +0000

    fix: `parseDate` should prefer the first matching format in array
    Fix involves vastly simplifying the internal code-paths of
    `parseDate`, to prevent further and repeated divergence of behavior
    when parsing `dateFormat` as `Array<string>` vs. as `string`
    
    NOTE: Removing the (redundant) `minDate` parameter has no effect on
    the tests, as minDate/maxDate boundry checks are enforced elsewhere
    in the component's value-updating lifecycle.
    
    NOTE 2: Adding instead `refDate` (using `props.selected`) to
    fully utilize the features of `date-fns/parse`.
    
    NOTE 3: The old behavior of re-parsing borked values using
    `new Date()` was somewhat dubious as it gave different results
    depending on the Browser/OS running the code.
    See more here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/Date#parameters

--- src/index.jsx
+++ src/index.jsx
@@ -496,6 +496,7 @@ export default class DatePicker extends React.Component {
       this.props.dateFormat,
       this.props.locale,
       this.props.strictParsing,
+      this.props.selected,
       this.props.minDate
     );
     // Use date from `selected` prop when manipulating only time for input value
