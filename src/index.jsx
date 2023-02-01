4d216ce04f17d02f90f1fb447aaf4749ffc6d05e
feba.baby <feba.baby@qburst.com>
2023-02-01 12:24:51 +0530

    fix #3525

--- src/index.jsx
+++ src/index.jsx
@@ -1182,7 +1182,6 @@ export default class DatePicker extends React.Component {
           className="react-datepicker__calender-icon"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 448 512"
-          onClick={this.renderDateInput()}
         >
           <path d="M96 32V64H48C21.5 64 0 85.5 0 112v48H448V112c0-26.5-21.5-48-48-48H352V32c0-17.7-14.3-32-32-32s-32 14.3-32 32V64H160V32c0-17.7-14.3-32-32-32S96 14.3 96 32zM448 192H0V464c0 26.5 21.5 48 48 48H400c26.5 0 48-21.5 48-48V192z" />
         </svg>
