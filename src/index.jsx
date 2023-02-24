32d1f1ee6f464cdfe63d011048e96154d0a6e52e
Kelatev <kelatev@gmail.com>
2023-02-24 13:27:44 +0200

    fix: always show date select

--- src/index.jsx
+++ src/index.jsx
@@ -254,6 +254,7 @@ export default class DatePicker extends React.Component {
     showFourColumnMonthYearPicker: PropTypes.bool,
     showYearPicker: PropTypes.bool,
     showQuarterYearPicker: PropTypes.bool,
+    showDateSelect: PropTypes.bool,
     showTimeSelect: PropTypes.bool,
     showTimeSelectOnly: PropTypes.bool,
     timeFormat: PropTypes.string,
@@ -537,7 +538,9 @@ export default class DatePicker extends React.Component {
       this.props.onChangeRaw(event);
     }
     this.setSelected(date, event, false, monthSelectedIn);
-    this.setState({ isRenderAriaLiveMessage: true });
+    if (this.props.showDateSelect) {
+      this.setState({ isRenderAriaLiveMessage: true });
+    }
     if (!this.props.shouldCloseOnSelect || this.props.showTimeSelect) {
       this.setPreSelection(date);
     } else if (!this.props.inline) {
@@ -979,6 +982,7 @@ export default class DatePicker extends React.Component {
         weekDayClassName={this.props.weekDayClassName}
         monthClassName={this.props.monthClassName}
         timeClassName={this.props.timeClassName}
+        showDateSelect={this.props.showDateSelect}
         showTimeSelect={this.props.showTimeSelect}
         showTimeSelectOnly={this.props.showTimeSelectOnly}
         onTimeChange={this.handleTimeChange}
@@ -1076,7 +1080,7 @@ export default class DatePicker extends React.Component {
             locale,
           }
         )}`;
-      } else {
+      } /*if (this.props.showDateSelect)*/ else {
         ariaLiveMessage = `Selected date: ${safeDateFormat(
           this.props.selected,
           {
@@ -1093,7 +1097,7 @@ export default class DatePicker extends React.Component {
         aria-live="polite"
         className="react-datepicker__aria-live"
       >
-        {this.state.isRenderAriaLiveMessage && ariaLiveMessage}
+        {ariaLiveMessage}
       </span>
     );
   };
@@ -1193,7 +1197,7 @@ export default class DatePicker extends React.Component {
             <path d="M96 32V64H48C21.5 64 0 85.5 0 112v48H448V112c0-26.5-21.5-48-48-48H352V32c0-17.7-14.3-32-32-32s-32 14.3-32 32V64H160V32c0-17.7-14.3-32-32-32S96 14.3 96 32zM448 192H0V464c0 26.5 21.5 48 48 48H400c26.5 0 48-21.5 48-48V192z" />
           </svg>
         )}
-        {this.renderAriaLiveRegion()}
+        {this.state.isRenderAriaLiveMessage && this.renderAriaLiveRegion()}
         {this.renderDateInput()}
         {this.renderClearButton()}
       </div>
