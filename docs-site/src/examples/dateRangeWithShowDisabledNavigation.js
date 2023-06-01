e7d7fc601a3905530677e1024bcd7ad4c436ac77
RawandDev <rawand.200091384037@gmail.com>
2023-06-01 22:32:44 +0300

    fix: fix selects range acts as default picker

--- docs-site/src/examples/dateRangeWithShowDisabledNavigation.js
+++ docs-site/src/examples/dateRangeWithShowDisabledNavigation.js
@@ -1,11 +1,21 @@
 () => {
-  const [startDate, setStartDate] = useState(null);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(null);
+  const onChange = (dates) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
   return (
     <DatePicker
       selected={startDate}
-      onChange={(date) => setStartDate(date)}
+      onChange={onChange}
       minDate={new Date()}
       maxDate={addMonths(new Date(), 5)}
+      startDate={startDate}
+      endDate={endDate}
+      selectsRange
+      inline
       showDisabledMonthNavigation
     />
   );
