0d1e38af89b383b2f141254cb2ee18d51856d7e7
Martijn Russchen <martijn@hackerone.com>
2023-10-09 20:05:11 +0200

    Revert unused changes

--- docs-site/src/index.js
+++ docs-site/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./components/App";
 
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById("root"));
