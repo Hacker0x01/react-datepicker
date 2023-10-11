75a584aaaeb2b36c41aaa630ab1875f18d6e3708
Martijn Russchen <martijn@hackerone.com>
2023-10-11 09:35:59 +0200

    Trying switching to different Enzyme adapter

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
 import Enzyme from "enzyme";
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import Adapter from "@cfaester/enzyme-adapter-react-18";
 
 Enzyme.configure({ adapter: new Adapter() });
