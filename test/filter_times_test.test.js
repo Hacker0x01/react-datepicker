3eb9a1bbf5c58543c2bca19bce659ff447903b07
Balaji Sridharan <balaji.sridharan@nyulangone.org>
2023-10-14 23:31:20 +0530

    Migrate Enzyme test cases to RTL in filter_times_test.js and include_times_test.js
    Migrated the test cases to RTL as mentioned by @martijnrusschen in the issue #4300 for React 18 upgrade

--- test/filter_times_test.test.js
+++ test/filter_times_test.test.js
@@ -1,18 +1,23 @@
 import React from "react";
+import { render } from "@testing-library/react";
 import { mount } from "enzyme";
 import { getHours } from "../src/date_utils";
 import TimeComponent from "../src/time";
 
 describe("TimeComponent", () => {
   it("should disable times matched by filterTime prop", () => {
-    const timeComponent = mount(
+    const { container: timeComponent } = render(
       <TimeComponent filterTime={(time) => getHours(time) !== 17} />,
     );
 
-    expect(
-      timeComponent.find(".react-datepicker__time-list-item--disabled"),
-    ).toHaveLength(2);
+    const disabledTimeItems = timeComponent.querySelectorAll(
+      ".react-datepicker__time-list-item--disabled",
+    );
+    expect(disabledTimeItems.length).toBe(2);
 
-    expect(timeComponent.find('[aria-disabled="true"]')).toHaveLength(2);
+    const ariaDisabledTimeItems = Array.from(disabledTimeItems).filter(
+      (time) => time.getAttribute("aria-disabled") === "true",
+    );
+    expect(ariaDisabledTimeItems.length).toBe(2);
   });
 });
