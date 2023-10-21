55ef1a7fb6a81ac2f01e637b516f8d2c3d290e74
Balaji Sridharan <balaji.sridharan@nyulangone.org>
2023-10-21 16:31:36 +0530

    refactor: Improve code readability and maintainability
    In this commit, I've mad the following enhancements to the code:
    1. Split the test-cases and move the test-case for checking the aria-disabled attribute to a separate it block.
    2. Changed the use of the magic number 4 to the length of the input array excludeTimes for better flexibility and maintainability.
    
    Closes #4334

--- test/exclude_times_test.test.js
+++ test/exclude_times_test.test.js
@@ -4,26 +4,37 @@ import { setTime, newDate } from "../src/date_utils";
 import DatePicker from "../src/index.jsx";
 
 describe("DatePicker", () => {
-  it("should disable times specified in excludeTimes props", () => {
-    const now = newDate();
+  let now, excludeTimes;
 
+  beforeEach(() => {
+    now = newDate();
+    excludeTimes = [
+      setTime(now, { hour: 17, minute: 0 }),
+      setTime(now, { hour: 18, minute: 30 }),
+      setTime(now, { hour: 19, minute: 30 }),
+      setTime(now, { hour: 17, minute: 30 }),
+    ];
+  });
+
+  it("should disable times specified in excludeTimes props", () => {
     const { container: datePicker } = render(
-      <DatePicker
-        open
-        showTimeSelect
-        excludeTimes={[
-          setTime(now, { hour: 17, minute: 0 }),
-          setTime(now, { hour: 18, minute: 30 }),
-          setTime(now, { hour: 19, minute: 30 }),
-          setTime(now, { hour: 17, minute: 30 }),
-        ]}
-      />,
+      <DatePicker open showTimeSelect excludeTimes={excludeTimes} />,
     );
 
     const disabledTimeItems = datePicker.querySelectorAll(
       ".react-datepicker__time-list-item--disabled",
     );
-    expect(disabledTimeItems.length).toBe(4);
+    expect(disabledTimeItems.length).toBe(excludeTimes.length);
+  });
+
+  it("should add aria-disabled to all the excluded times", () => {
+    const { container: datePicker } = render(
+      <DatePicker open showTimeSelect excludeTimes={excludeTimes} />,
+    );
+
+    const disabledTimeItems = datePicker.querySelectorAll(
+      ".react-datepicker__time-list-item--disabled",
+    );
 
     const allDisabledTimeItemsHaveAriaDisabled = Array.from(
       disabledTimeItems,
