dc960ef4aac4152100ad6f34463685d6a30296b2
yuki0410-dev <>
2024-03-05 23:16:02 +0900

    fix enzyme to RTL (time_input_test.test.js)

--- test/time_input_test.test.js
+++ test/time_input_test.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { mount, shallow } from "enzyme";
+import { render, fireEvent } from "@testing-library/react";
 import DatePicker from "../src/index.jsx";
 import InputTimeComponent from "../src/inputTime.jsx";
 import CustomTimeInput from "./helper_components/custom_time_input.jsx";
@@ -10,51 +10,53 @@ describe("timeInput", () => {
   });
 
   it("should show time component when showTimeSelect prop is present", () => {
-    const datePicker = mount(<DatePicker showTimeInput />);
-    const component = datePicker.find(InputTimeComponent);
+    const { container } = render(<DatePicker showTimeInput open />);
+    const component = container.querySelector(
+      ".react-datepicker__input-time-container",
+    );
     expect(component).not.toBeNull();
   });
 
   it("should have custom time caption", () => {
-    const timeComponent = mount(
+    const { container } = render(
       <InputTimeComponent timeInputLabel="Custom time" />,
     );
-    const caption = timeComponent.find(".react-datepicker-time__caption");
-    expect(caption.text()).toEqual("Custom time");
+    const caption = container.querySelector(".react-datepicker-time__caption");
+    expect(caption.textContent).toEqual("Custom time");
   });
 
   xit("should trigger onChange event", () => {
-    const timeComponent = shallow(<InputTimeComponent />);
-    const input = timeComponent.find("input");
-    input.simulate("change", { target: { value: "13:00" } });
-    expect(timeComponent.state("time")).toEqual("13:00");
+    const { container } = render(<InputTimeComponent />);
+    const input = container.querySelector("input");
+    fireEvent.change(input, { target: { value: "13:00" } });
+    expect(input.value).toEqual("13:00");
   });
 
   it("should trigger onChange event and set the value as last valid timeString if empty string is passed as time input value", () => {
-    const timeComponent = shallow(
+    const { container } = render(
       <InputTimeComponent timeString="13:00" onChange={() => {}} />,
     );
-    const input = timeComponent.find("input");
-    input.simulate("change", { target: { value: "" } });
-    expect(timeComponent.state("time")).toEqual("13:00");
+    const input = container.querySelector("input");
+    fireEvent.change(input, { target: { value: "" } });
+    expect(input.value).toEqual("13:00");
   });
 
   xit("should trigger onChange event on a custom time input without using the last valid timeString", () => {
-    const timeComponent = mount(
+    const { container } = render(
       <InputTimeComponent
         timeString="13:00"
         customTimeInput={<CustomTimeInput />}
       />,
     );
 
-    const input = timeComponent.find("CustomTimeInput");
-    input.simulate("change", "14:00");
-    expect(timeComponent.state("time")).toEqual("14:00");
+    const input = container.querySelector("input");
+    fireEvent.change(input, { target: { value: "14:00" } });
+    expect(input.value).toEqual("14:00");
   });
 
   xit("should pass pure Date to custom time input", () => {
     const onTimeChangeSpy = jest.fn();
-    const timeComponent = mount(
+    const { container } = render(
       <InputTimeComponent
         date={new Date()}
         timeString="13:00"
@@ -62,8 +64,8 @@ describe("timeInput", () => {
       />,
     );
 
-    const input = timeComponent.find(CustomTimeInput);
-    input.simulate("change", "14:00");
+    const input = container.querySelector("input");
+    fireEvent.change(input, { target: { value: "14:00" } });
 
     expect(onTimeChangeSpy).toHaveBeenCalledWith(
       new Date(new Date().setHours(14, 0, 0, 0)),
@@ -74,13 +76,13 @@ describe("timeInput", () => {
     const mockOnChange = jest.fn();
     const mockDate = new Date("2023-09-30");
 
-    const timeComponent = shallow(
+    const { container } = render(
       <InputTimeComponent date={mockDate} onChange={mockOnChange} />,
     );
 
     const newTime = "13:00";
-    const input = timeComponent.find("input");
-    input.simulate("change", { target: { value: newTime } });
+    const input = container.querySelector("input");
+    fireEvent.change(input, { target: { value: newTime } });
 
     const expectedDate = new Date(mockDate);
     const [expectedHours, expectedMinutes] = newTime.split(":");
@@ -97,13 +99,13 @@ describe("timeInput", () => {
       .spyOn(global, "Date")
       .mockImplementation(() => mockCurrentDate);
 
-    const timeComponent = shallow(
+    const { container } = render(
       <InputTimeComponent onChange={mockOnChange} />,
     );
 
     const newTime = "13:00";
-    const input = timeComponent.find("input");
-    input.simulate("change", { target: { value: newTime } });
+    const input = container.querySelector("input");
+    fireEvent.change(input, { target: { value: newTime } });
 
     const expectedDate = new Date(mockCurrentDate);
     const [expectedHours, expectedMinutes] = newTime.split(":");
