1aa29ee48893ca945e0cf230fd9c74a2056d2164
yuki0410-dev <>
2024-03-07 00:37:04 +0900

    fix enzyme to RTL (week_picker_test.js => week_picker_test.test.js)

---
+++ test/week_picker_test.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import DatePicker from "../src/index.jsx";
+import { render, fireEvent } from "@testing-library/react";
+
+describe("WeekPicker", () => {
+  it("should change the week when clicked on any option in the picker", () => {
+    const onChangeSpy = jest.fn();
+    const { container } = render(
+      <DatePicker onChange={onChangeSpy} showWeekPicker />,
+    );
+    expect(onChangeSpy).not.toHaveBeenCalled();
+    fireEvent.focus(container.querySelector("input"));
+    fireEvent.click(container.querySelector(".react-datepicker__day"));
+    expect(onChangeSpy).toHaveBeenCalled();
+  });
+
+  it("should change the week when clicked on any week number in the picker", () => {
+    const onChangeSpy = jest.fn();
+    const { container } = render(
+      <DatePicker onChange={onChangeSpy} showWeekPicker showWeekNumbers />,
+    );
+    expect(onChangeSpy).not.toHaveBeenCalled();
+    fireEvent.focus(container.querySelector("input"));
+    fireEvent.click(container.querySelector(".react-datepicker__week-number"));
+    expect(onChangeSpy).toHaveBeenCalled();
+  });
+});
