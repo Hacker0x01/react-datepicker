61c20abf820427932a2727620356bdee4ccc832a
Mirus <mirusim@gmail.com>
2024-04-20 11:17:05 +0300

    docs: jsdoc and a bit better type

--- src/with_floating.tsx
+++ src/with_floating.tsx
@@ -7,15 +7,32 @@ import {
   autoUpdate,
   Middleware,
   Placement,
+  type UseFloatingOptions,
 } from "@floating-ui/react";
 
 interface WithFloatingProps {
   popperModifiers?: Middleware[];
-  popperProps?: object;
+  popperProps?: Omit<UseFloatingOptions, "middleware">;
   hidePopper?: boolean;
   popperPlacement?: Placement;
 }
 
+/**
+ * `withFloating` is a higher-order component that adds floating behavior to a component.
+ *
+ * @param Component - The component to enhance.
+ *
+ * @example
+ * const FloatingComponent = withFloating(MyComponent);
+ * <FloatingComponent popperModifiers={[]} popperProps={{}} hidePopper={true} />
+ *
+ * @param popperModifiers - The modifiers to use for the popper.
+ * @param popperProps - The props to pass to the popper.
+ * @param hidePopper - Whether to hide the popper.
+ * @param popperPlacement - The placement of the popper.
+ *
+ * @returns A new component with floating behavior.
+ */
 export default function withFloating<T extends {}>(
   Component: ComponentType<T>
 ) {
