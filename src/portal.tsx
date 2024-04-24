c97db58ec61b0c6b85d4095cc4cdf26cef843c2b
Olenka <alyonayurchuk@gmail.com>
2024-04-24 14:44:35 +0300

    feat: added jsdoc to portal

--- src/portal.tsx
+++ src/portal.tsx
@@ -7,6 +7,16 @@ interface PortalProps {
   portalHost?: ShadowRoot;
 }
 
+/**
+ * `Portal` is a React component that allows you to render children into a DOM node
+ * that exists outside the DOM hierarchy of the parent component.
+ *
+ * @class
+ * @param {PortalProps} props - The properties that define the `Portal` component.
+ * @property {React.ReactNode} props.children - The children to be rendered into the `Portal`.
+ * @property {string} props.portalId - The id of the DOM node into which the `Portal` will render.
+ * @property {ShadowRoot} [props.portalHost] - The DOM node to host the `Portal`.
+ */
 class Portal extends Component<PortalProps> {
   private el: HTMLDivElement;
   private portalRoot!: HTMLElement | null;
