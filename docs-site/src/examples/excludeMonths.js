a4ca0644437bca23deaecd69c4a4d1dbbb0adc9c
ryantanrk <ryantanrk@gmail.com>
2024-05-20 09:34:54 +0800

    clean up excludeMonths.js

--- docs-site/src/examples/excludeMonths.js
+++ docs-site/src/examples/excludeMonths.js
@@ -1,21 +1,15 @@
 () => {
-  const defaultDate = new Date("2024-08-01");
-  const [startDate, setStartDate] = useState(defaultDate);
+  const [startDate, setStartDate] = useState(new Date());
   return (
     <DatePicker
       selected={startDate}
       onChange={(date) => setStartDate(date)}
       excludeDates={[
-        new Date("2024-05-01"),
-        new Date("2024-02-01"),
-        new Date("2024-01-01"),
-        new Date("2024-11-01")
+        subMonths(new Date(), 2),
       ]}
       dateFormat="MM/yyyy"
       placeholderText="Select a month other than the disabled months"
       showMonthYearPicker
-      minDate={new Date("2023-06-01")}
-      maxDate={new Date("2025-02-01")}
     />
   );
 };
