5e96014d23548544d5a3a2ceb8d763cdcef58f97
ezParth <parthsingh55m@gmail.com>
2024-07-05 17:29:17 +0530

    Added one click toggle of custom time input bar

--- docs-site/src/examples/customTimeInput.js
+++ docs-site/src/examples/customTimeInput.js
@@ -1,12 +1,20 @@
 () => {
   const [startDate, setStartDate] = useState(new Date());
-  const ExampleCustomTimeInput = ({ date, value, onChange }) => (
-    <input
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-      style={{ border: "solid 1px pink" }}
-    />
-  );
+  const ExampleCustomTimeInput = ({ date, value, onChange }) => {
+    const handleInputClick = () => {
+      inputRef.current.focus();
+    };
+    const inputRef = React.createRef(); 
+    return (
+      <input
+        ref={inputRef}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        onClick={handleInputClick}
+        style={{ border: "solid 1px pink" }}
+      />
+    );
+  };
   return (
     <DatePicker
       selected={startDate}
