0557c3cc2cec56ee6e648e067ee42373bba3f56b
Galin Malchev <g.malchev@live.com>
2024-07-20 14:11:29 +0300

    in the docs examples - customInput - the 'react-datepicker-ignore-onclickouts' was not applied to custom inputs leading to closing and opening flickering when clicked on the custom imput element

--- docs-site/src/examples/customInput.js
+++ docs-site/src/examples/customInput.js
@@ -1,15 +1,17 @@
 () => {
   const [startDate, setStartDate] = useState(new Date());
-  const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
-    <button className="example-custom-input" onClick={onClick} ref={ref}>
-      {value}
-    </button>
-  ));
+  const ExampleCustomInput = forwardRef(
+    ({ value, onClick, className }, ref) => (
+      <button className={className} onClick={onClick} ref={ref}>
+        {value}
+      </button>
+    ),
+  );
   return (
     <DatePicker
       selected={startDate}
       onChange={(date) => setStartDate(date)}
-      customInput={<ExampleCustomInput />}
+      customInput={<ExampleCustomInput className="example-custom-input" />}
     />
   );
 };
