70694855e2db0968faff8b646e3145bb12903541
Oleg <67059482+OlegDev1@users.noreply.github.com>
2024-11-07 13:47:04 +0000

    fix: "previous month" button appears when selecting a date

--- src/test/calendar_test.test.tsx
+++ src/test/calendar_test.test.tsx
@@ -448,6 +448,27 @@ describe("Calendar", () => {
     expect(nextButtonAriaLabel).toBe(nextYearAriaLabel);
   });
 
+  it("should not have previous month button when selecting a date in the second month, when min date is specified", () => {
+    const { container } = render(
+      <DatePicker
+        inline
+        monthsShown={2}
+        minDate={new Date("2024-11-06")}
+        maxDate={new Date("2025-01-01")}
+      />,
+    );
+
+    const secondMonthDate = safeQuerySelectorAll(
+      container,
+      ".react-datepicker__day--009",
+    )[1];
+    fireEvent.click(secondMonthDate!);
+
+    expect(
+      container.querySelector(".react-datepicker__navigation--previous"),
+    ).toBe(null);
+  });
+
   describe("custom header", () => {
     const months = [
       "January",
